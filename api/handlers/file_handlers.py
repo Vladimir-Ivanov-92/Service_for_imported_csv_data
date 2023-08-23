dab976dc86f7c509fd7041d8f8be41ebe9eddcee
vova2092 <vovai2092@yandex.ru>
2023-08-23 16:32:35 +0300

    Add possibility of filtering by columns. After filtering, the data can be sorted.

--- api/handlers/file_handlers.py
+++ api/handlers/file_handlers.py
@@ -42,17 +42,42 @@ async def show_tables(request: Request, db: AsyncSession = Depends(get_db)):
 
 ASCENDING_STATE = True
 
-# Отображение выбранной таблицы (с возможностью сортироки)
+
 @file_router.get('/table/{table_name}', response_class=HTMLResponse)
 async def sort_data(request: Request, table_name: str,
                     sort_column: str = Query(None, alias="sort_column"),
                     ascending: bool = Query(None, alias="ascending"),
+                    filter_column: str = Query(None, alias="filter_column"),
+                    filter_value: str = Query(None, alias="filter_value"),
                     db: AsyncSession = Depends(get_db)):
     global ASCENDING_STATE
 
     file_path = f"uploads/{table_name}.csv"
     table = pd.read_csv(file_path)
     name_columns = await _get_name_columns_from_csv_table(table, db)
+    numeric_columns = table.select_dtypes(include=['int64', 'float64']).columns.tolist()
+    columns_data = table
+
+    if filter_column:
+        # Применяем фильтрацию
+        filter_column_values = table[filter_column].unique()
+        filter_values = filter_column_values.tolist()
+
+        if filter_value:
+            filter_value = filter_value
+            if filter_column in numeric_columns:
+                try:
+                    filter_value = float(
+                        filter_value)  # Преобразуем значение в числовой тип
+                    columns_data = table[table[filter_column] == filter_value]
+                except ValueError:
+                    pass
+            else:
+                columns_data = table[table[filter_column] == filter_value]
+        else:
+            columns_data = table  # Если некорректное значение фильтра, показываем все данные
+    else:
+        filter_values = []
 
     if sort_column:
         if ascending is None:
@@ -60,13 +85,17 @@ async def sort_data(request: Request, table_name: str,
         else:
             ASCENDING_STATE = not ASCENDING_STATE
 
-        columns_data = table.sort_values(by=sort_column,
-                                         ascending=ASCENDING_STATE).iterrows()
+        # Применяем сортировку к отфильтрованным данным
+        columns_data = columns_data.sort_values(by=sort_column,
+                                                ascending=ASCENDING_STATE)
     else:
-        columns_data = table.iterrows()
+        columns_data = columns_data
 
     context = {"request": request, "table_name": table_name,
-               "name_columns": name_columns, "columns_data": columns_data,
+               "name_columns": name_columns, "columns_data": columns_data.iterrows(),
                "sort_column": sort_column,
+               "filter_column": filter_column,
+               "filter_value": filter_value,
+               "filter_values": filter_values,
                "ascending": ASCENDING_STATE}
     return templates.TemplateResponse("data.html", context)
